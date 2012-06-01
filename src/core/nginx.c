aceb1f3a3b7bab51db650b2ba0ecacdaabaa82e7
Weibin Yao <yaoweibin@gmail.com>
2012-06-01 13:52:24 +0800

    merge the dso patch

--- src/core/nginx.c
+++ src/core/nginx.c
@@ -187,9 +187,9 @@ ngx_module_t  ngx_core_module = {
 ngx_uint_t          ngx_max_module;
 ngx_uint_t          ngx_dump_config;
 
+
 static ngx_uint_t   ngx_show_help;
 static ngx_uint_t   ngx_show_version;
-static ngx_uint_t   ngx_show_modules;
 static ngx_uint_t   ngx_show_configure;
 static ngx_uint_t   ngx_show_directives;
 static u_char      *ngx_prefix;
@@ -272,14 +272,6 @@ main(int argc, char *const *argv)
                 "configure arguments:" NGX_CONFIGURE NGX_LINEFEED);
         }
 
-        if (ngx_show_modules) {
-            ngx_log_stderr(0, "compiled in modules:");
-
-            for (i = 0; ngx_module_names[i]; i++) {
-                ngx_log_stderr(0, "    %s", ngx_module_names[i]);
-            }
-        }
-
         if (ngx_show_directives) {
             ngx_log_stderr(0, "all available directives:");
 
@@ -297,7 +289,7 @@ main(int argc, char *const *argv)
             }
         }
 
-        if(!ngx_test_config) {
+        if(!ngx_test_config && !ngx_show_modules) {
             return 0;
         }
     }
